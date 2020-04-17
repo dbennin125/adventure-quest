b532d7438fece82bf594101c05faa9cd39720efb
Daniel Bennington <dbennington125@gmail.com>
2020-04-16 17:12:21 -0700

    so far

---
+++ test/makeUser.test.js
@@ -0,0 +1,28 @@
+import makeUser from '../makeUser.js';
+// IMPORT MODULES under test here:
+// import example from '../src/example.js';
+
+const test = QUnit.test;
+
+test('testing makeUser function', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const formData = new FormData();
+    
+    formData.set('name', 'dani');
+    formData.set('class', 'ranger');
+
+    const expected = {
+        name: 'dani',
+        class: 'ranger',
+        completed: {},
+        HP: 35,
+        gold: 0,
+    }; 
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const userObject = makeUser(formData);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.deepEqual(expected, userObject);
+});
